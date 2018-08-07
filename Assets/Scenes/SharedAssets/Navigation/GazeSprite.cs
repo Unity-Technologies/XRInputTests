c39ec6fda6a1f6b8d0bd99f85e6b414a94f45a26
JackPr <jackpr@unity3d.com>
2018-08-07 14:26:47 -0700

    Adding navigation arrows to 2018.3

---
+++ Assets/Scenes/SharedAssets/Navigation/GazeSprite.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeSprite : MonoBehaviour {
+
+    public Transform[] m_GazeableTransforms;
+    public float m_Angle;
+    private MeshRenderer m_MeshRenderer;
+    private Material m_Material;
+    public Color m_Color;
+
+    public float m_Distance = 2.0f;
+    
+
+	// Use this for initialization
+	void Start () {
+        MeshRenderer m_MeshRenderer = GetComponent<MeshRenderer>();
+        if(m_MeshRenderer != null)
+        {
+            m_Material = m_MeshRenderer.material;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (m_Material != null)
+        {
+            float bestAngle = float.MaxValue;
+
+            foreach (Transform t in m_GazeableTransforms)
+            {
+                Vector3 dir = (t.transform.position - Camera.main.transform.position).normalized;
+                float angle = Mathf.Abs(Vector3.Angle(dir, Camera.main.transform.forward));
+
+                if (angle < bestAngle)
+                {
+                    bestAngle = angle;
+                }
+            }
+            
+            float delta = m_Angle - bestAngle;
+            float lerpFactor = 0.0f;
+            if(delta > 0f)
+            {
+                lerpFactor = 1.0f - (bestAngle / m_Angle);
+            }
+            
+            m_Material.SetColor("_TintColor", Color.Lerp(Color.clear, m_Color, lerpFactor));
+
+            if (m_MeshRenderer != null)
+            {
+                m_MeshRenderer.enabled = lerpFactor > 0f;
+            }
+        }
+
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = Camera.main.transform.position + Camera.main.transform.forward * m_Distance;
+        transform.forward = -Camera.main.transform.forward;
+    }
+}
---
+++ Assets/Scenes/SharedAssets/Navigation/NavigationArrow.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationArrow : MonoBehaviour {
+
+    private Material m_Material;
+    private Collider m_Collider;
+
+    private float m_Timer = 0.0f;
+    public float m_MinActiveValue = 0.2f;
+    public float m_MaxTimer = 2.0f;
+
+    public float m_SceneIncrement = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if(meshRenderer != null)
+        {
+            m_Material = meshRenderer.material;
+        }
+        m_Collider = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_Collider != null && !Navigation.loadingScene)
+        {
+            RaycastHit hitInfo;
+            bool hit = m_Collider.Raycast(new Ray(Camera.main.transform.position, Camera.main.transform.forward), out hitInfo, float.MaxValue);
+            float offset = hit ? Time.deltaTime : -Time.deltaTime;
+
+            m_Timer += offset;
+
+            if(offset > 0f && m_Timer < m_MinActiveValue)
+            {
+                m_Timer = m_MinActiveValue;
+            }
+
+            m_Timer = Mathf.Clamp(m_Timer, 0.0f, m_MaxTimer);
+            
+            float t = m_Timer / m_MaxTimer;
+
+            m_Material.SetFloat("_LerpFactor", t);
+
+            if(m_Timer >= m_MaxTimer)
+            {
+                if(Mathf.Sign(m_SceneIncrement) > 0.0f)
+                {
+                    Navigation.NextScene();
+                }
+                else
+                {
+                    Navigation.PreviousScene();
+                }
+
+                m_Timer = 0;
+            }
+        }
+	}
+}
