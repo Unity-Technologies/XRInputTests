f611f03704090be025d1af42b44faeae0507b2c8
JackPr <jackpr@unity3d.com>
2018-09-26 11:42:38 -0700

    New WMR branch to make issue reproduction easy

---
+++ Assets/Persistent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persistent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
