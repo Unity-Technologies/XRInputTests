99317a80ca1c84edd91103439290c20a565e3ba8
JackPr <jackpr@unity3d.com>
2019-03-08 14:36:02 -0800

    Adding HistoricInput tests

---
+++ Assets/Tests/HistoricInput/ControllerHistory.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR.Interaction;
+using UnityEngine.XR;
+
+public class ControllerHistory : BasePoseProvider
+{
+    public double TimeBackwards = 50;
+    public XRNode DeviceToTrack;
+
+    public override bool TryGetPoseFromProvider(out Pose output)
+    {
+        var device = InputDevices.GetDeviceAtXRNode(DeviceToTrack);
+        Vector3 position = Vector3.zero;
+        Quaternion rotation = Quaternion.identity;
+        DateTime time = new DateTime();
+        time = DateTime.Now;
+        time = time.AddMilliseconds(-TimeBackwards);
+
+        if(device.TryGetFeatureValue(CommonUsages.deviceRotation, time, out rotation))
+        {
+            Debug.Log("Frame " + Time.frameCount + " " + DeviceToTrack + " rotation at time " + DateTime.Now.Millisecond + " - " + TimeBackwards + "= (" + time.Millisecond + ") is " + rotation.w + ", " + rotation.x + ", " + rotation.y + ", " + rotation.z);
+
+            device.TryGetFeatureValue(CommonUsages.devicePosition, time, out position);          
+            output.position = position;
+            output.rotation = rotation;
+            return true;            
+        }        
+        else
+        {
+            output = Pose.identity;
+            return false;
+        }
+    }
+}
