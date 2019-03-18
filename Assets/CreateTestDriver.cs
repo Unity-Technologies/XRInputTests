72fa74dd3a92982161b3f6fbd5e7520b641f1eea
Jack Pritz <jackpr@unity3d.com>
2019-03-18 14:33:32 -0700

    Remove Start scene

---
+++ Assets/CreateTestDriver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTestDriver : MonoBehaviour
+{
+    public GameObject TestDriver;
+
+    private void Awake()
+    {
+        if (GameObject.FindGameObjectWithTag("TestDriver") == null)
+        {
+            Debug.Log("Creating new test driver");
+            Instantiate(TestDriver);
+        }
+    }
+}
