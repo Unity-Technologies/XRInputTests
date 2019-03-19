b6233899e04d2365b4c4d1afa0f61e051b211bb9
Jack Pritz <jackpr@unity3d.com>
2019-03-18 17:46:14 -0700

    Add AR test driver

--- Assets/CreateTestDriver.cs
+++ Assets/CreateTestDriver.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class CreateTestDriver : MonoBehaviour
 {
     public GameObject TestDriver;
+    public GameObject TestDriver_AR;
+
+    public bool AR;
 
     private void Awake()
     {
         if (GameObject.FindGameObjectWithTag("TestDriver") == null)
         {
             Debug.Log("Creating new test driver");
-            Instantiate(TestDriver);
+            if (AR)
+                Instantiate(TestDriver_AR);
+            else
+                Instantiate(TestDriver);
         }
     }
 }
