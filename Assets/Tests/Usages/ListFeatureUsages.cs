46df4fec34e5def1df56b599e022aa14000e00d9
JackPr <jackpr@unity3d.com>
2020-02-04 10:14:42 -0800

    Add value reporting to list of feature usages for testing non common usages

--- Assets/Tests/Usages/ListFeatureUsages.cs
+++ Assets/Tests/Usages/ListFeatureUsages.cs
@@ -10,10 +10,12 @@ public class ListFeatureUsages : MonoBehaviour
     public XRNode node;
     public Text listText;
 
+    public float delayTime = 1f;
+
     void Start()
     {
         ShowFeatures();
-        StartCoroutine("UpdateFeatureNamesEveryDelay", 3);
+        StartCoroutine("UpdateFeatureNamesEveryDelay", delayTime);
     }
 
     IEnumerator UpdateFeatureNamesEveryDelay(float delayInSeconds)
@@ -40,7 +42,21 @@ public class ListFeatureUsages : MonoBehaviour
             if (device.TryGetFeatureUsages(features))
                 foreach (InputFeatureUsage feature in features)
                 {
-                    displayTextAccumulator += ("<" + nodeNumber + "> " + feature.name + " - \"" + feature.type.ToString() + "\"\n");
+                    displayTextAccumulator += ("<" + nodeNumber + "> " + feature.name + " ");
+
+                    // Can't use switch statement because types are not considered constant
+                    if (feature.type == typeof(bool))
+                        displayTextAccumulator += TextAccumBool(device, feature);
+                    if (feature.type == typeof(float))
+                        displayTextAccumulator += TextAccumFloat(device, feature);
+                    if (feature.type == typeof(Vector2))
+                        displayTextAccumulator += TextAccumVector2(device, feature);
+                    if (feature.type == typeof(Vector3))
+                        displayTextAccumulator += TextAccumVector3(device, feature);
+                    if (feature.type == typeof(uint))
+                        displayTextAccumulator += TextAccumUint(device, feature);
+
+                    displayTextAccumulator += ("\n");
                     nodeNumber++;
                 }
             else
@@ -48,5 +64,36 @@ public class ListFeatureUsages : MonoBehaviour
         }
 
         listText.text = displayTextAccumulator;
+    }
+    
+    string TextAccumBool(InputDevice device, InputFeatureUsage feature)
+    {
+        bool value;
+        device.TryGetFeatureValue(feature.As<bool>(), out value);
+        return value.ToString();
+    }
+    string TextAccumFloat(InputDevice device, InputFeatureUsage feature)
+    {
+        float value;
+        device.TryGetFeatureValue(feature.As<float>(), out value);
+        return value.ToString();
+    }
+    string TextAccumVector2(InputDevice device, InputFeatureUsage feature)
+    {
+        Vector2 value;
+        device.TryGetFeatureValue(feature.As<Vector2>(), out value);
+        return value.ToString();
+    }
+    string TextAccumVector3(InputDevice device, InputFeatureUsage feature)
+    {
+        Vector3 value;
+        device.TryGetFeatureValue(feature.As<Vector3>(), out value);
+        return value.ToString();
+    }
+    string TextAccumUint(InputDevice device, InputFeatureUsage feature)
+    {
+        uint value;
+        device.TryGetFeatureValue(feature.As<uint>(), out value);
+        return value.ToString();
     }
 }
